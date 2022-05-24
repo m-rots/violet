14ad546360fca179948db4cf95da2295ede3b158
Storm Timmermans <stormtimmermans@icloud.com>
2022-05-24 11:19:32 +0200

    docs: update minimal example

--- README.md
+++ README.md
@@ -23,10 +23,7 @@ from vi import Agent, Simulation
 
 (
     Simulation()
-    .batch_spawn_agents(
-        Agent,
-        image_paths=["images/white.png"],
-    )
+    .batch_spawn_agents(Agent, image_paths=["examples/images/white.png"])
     .run()
 )
 ```
--- examples/minimal.py
+++ examples/minimal.py
@@ -2,9 +2,6 @@ from vi import Agent, Simulation
 
 (
     Simulation()
-    .batch_spawn_agents(
-        Agent,
-        image_paths=["examples/images/white.png"],
-    )
+    .batch_spawn_agents(Agent, image_paths=["examples/images/white.png"])
     .run()
 )
