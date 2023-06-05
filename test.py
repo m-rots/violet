3109722bfc7f794cbd3f571eb1a9fb5df4078e65
Younes-Azzouz <111754250+Younes-Azzouz@users.noreply.github.com>
2023-06-05 17:28:35 +0200

    Create test.py

---
+++ test.py
@@ -0,0 +1,11 @@
+
+from vi import Agent, Simulation
+
+(
+    # Step 1: Create a new simulation.
+    Simulation()
+    # Step 2: Add 100 agents to the simulation.
+    .batch_spawn_agents(100, Agent, images=["examples/images/white.png"])
+    # Step 3: Profit! 🎉
+    .run()
+)
